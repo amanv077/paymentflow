ebc96d3f33f384f962ac0967e4179877718650cd
aman <amanverma4816@gmail.com>
2024-09-30 00:23:17 +0530

    payment

--- src/paymentflow/PaymentInit.tsx
+++ src/paymentflow/PaymentInit.tsx
@@ -5,8 +5,8 @@ export default function PaymentInit({
   data: { amount },
 }: NodeProps<{ amount: number }>) {
   return (
-    <div className="bg-white border border-purple-600">
-      <div className="bg-purple-900 p-1">
+    <div className="bg-white border border-green-600">
+      <div className="bg-green-900 p-1">
         <p className="text-sm text-white">Payment Initialized</p>
       </div>
       <div className="p-2">
